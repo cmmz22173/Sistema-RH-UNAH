09e4b8980e9e6217de3f4a109559691abb6badee
cmmz22173 <69021120+cmmz22173@users.noreply.github.com>
2022-09-28 20:10:51 -0600

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <LoginPage></LoginPage>
+</template>
+<script>
+  import LoginPage from './components/LoginPage.vue'
+
+  export default {
+    name: 'App',
+    components:{
+      LoginPage
+    }
+  }
+</script>
